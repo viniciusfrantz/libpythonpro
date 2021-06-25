33d7443d4aa197b3f90f3a04b0a48a2a3b29741f
viniciusfrantz <viniciusfrantz@hotmail.com>
2021-06-25 11:34:06 -0300

    Acrescentada cobertur de código

---
+++ libpythonpro/tests/test_spam/test_enviador_de_email.py
@@ -0,0 +1,39 @@
+import pytest
+
+from libpythonpro.spam.enviador_de_email import Enviador, EmailInvalido
+
+
+def test_criar_enviador_de_email():
+    enviador = Enviador()
+    assert enviador is not None
+
+@pytest.mark.parametrize(
+    'destinatario',
+    ['viniciusfrantz@hotmail.com','joao@hotmail.com']
+)
+def test_remetente(destinatario):
+    enviador = Enviador()
+
+    resultado=enviador.enviar(
+        destinatario,
+        'destinatario@hotmail.com',
+        'Curso Python Pro',
+        'Turma Henrique Bastos'
+    )
+    assert destinatario in resultado
+
+
+@pytest.mark.parametrize(
+    'remetente',
+    ['aaahotmail.com','']
+)
+def test_remetente_invalido(remetente):
+    enviador = Enviador()
+    with pytest.raises(EmailInvalido):
+        enviador.enviar(
+            remetente,
+            'destinatario@hotmail.com',
+            'Curso Python Pro',
+            'Turma Henrique Bastos'
+        )
+    assert remetente in resultado
